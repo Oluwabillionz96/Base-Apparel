0fc3f62a8a4ae1cf1208d66f7ad160e4183a9443
oluwabillionz96 <goodluckreuben96@gmail.com>
2024-10-15 07:43:21 -0700

    project complete

---
+++ index.js
@@ -0,0 +1,24 @@
+// Getting Elements
+const button = document.getElementById('send-icon');
+const error = document.querySelector('.error-i');
+const errorMessage = document.getElementById('error');
+const inputBorder = document.querySelector('.input-wrapper');
+const input = document.querySelector('.maill'); 
+console.log(input);
+
+// Adding Event listeners and verifying
+
+button.addEventListener('click', function(){
+
+    if(input.value.includes('@') === true && input.value.includes('.com') === true){
+       inputBorder.style.borderColor = 'green';
+       input.value = '';
+       errorMessage.style.display = 'none';
+       error.style.display = 'none';
+    } else if(input.value === ''|| input.value.includes('@') === false || input.value.includes('.com') === false){
+        errorMessage.style.display = 'block';
+        inputBorder.style.borderColor = 'red';
+        error.style.display = 'inline';
+    }
+    }) 
+
